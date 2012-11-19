0840425487284ee82283a418f7fe231cba55959c
Sergey Bartunov <sbartunov@gmail.com>
2012-11-19 14:02:33 +0400

    The very first commit

---
+++ src/main/scala/ru/dreamindustries/topicmodeling/LDA.scala
@@ -0,0 +1,12 @@
+package ru.dreamindustries.topicmodeling
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sbos
+ * Date: 11/19/12
+ * Time: 1:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class LDA {
+
+}
