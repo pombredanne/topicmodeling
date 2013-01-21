fbac40a308398e090f98ac3dd373dabc024d6e3f
sbos <sbos.net@gmail.com>
2013-01-21 16:26:05 +0400

    Assumed to be working correctly collapsed zero-order variational inference

--- LDA.py
+++ LDA.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.sparse import csr_matrix
+from scipy.sparse import csr_matrix, csc_matrix
 from numpy.random import randn
 from sklearn.preprocessing import normalize
 
@@ -14,11 +14,15 @@ def TCVB0(docs, alpha, beta, epsilon=0.00001):
     K = len(alpha)
 
     #store variational q_{z_{d,w} = t} for each d as sparse table in
-    #array q_z
-    q_z = np.zeros(D, dtype=object)
+    #array z
+    z = np.zeros(D, dtype=object)
 
     #initialize counts
+    #N[t, w] = expectaction of phi_{k,w}
     N = np.zeros((K, V), dtype=float)
+
+    #Nd[d, t] = unnormalized theta_{d,k}
+    Nd = np.zeros((D, K), dtype=float)
 
     for d in xrange(D):
         #random initialization
@@ -27,16 +31,53 @@ def TCVB0(docs, alpha, beta, epsilon=0.00001):
         ij[0] = np.tile(docs[d].nonzero()[1], K)
         ij[1] = np.tile(np.arange(K), V)
 
-        #q_z[d] is VxK sparse row matrix
-        q_z[d] = csr_matrix((init, ij), shape=(V, K), dtype=float)
+        #z[d] is VxK sparse row matrix
+        z[d] = csc_matrix((init, ij), shape=(V, K), dtype=float)
 
-        #normalize q_z[d]
-        q_z[d] = normalize(q_z[d], norm='L1', axis=1)
+        #normalize z[d]
+        z[d] = normalize(z[d], norm='L1', axis=1)
 
         for t in xrange(K):
-            N[t] += docs[d].transpose(copy=False).multiply(q_z[d][:, t].transpose(copy=False))       
+            N[t] += docs[d].multiply(z[d][:, t])
+            Nd[t] = np.dot(docs[d], z[d][:, t])
+
+        z[d] = z[d].to_csr()
+
+    #Nt[t] is pre-computed expectation topic t
+    Nt = N.sum(axis=1)
 
     #do variational updates until convergence
     while True:
+        """Performs variational update for document `d` and word `w`
+        """
         def variational_update(d, w):
-            
+            old_z = z[d][w]
+            #we take expectations ignoring current document and current word
+            N[:, w] -= old_z
+            Nt -= old_z
+            Nd[d] -= old_z
+            #update
+            z[d][w] = (N[:, w] + beta) / (Nt + V * beta) \
+                * (Nd[d] + alpha)
+            #normalization
+            z[d][w] /= z[d][w].sum()
+            #counts update
+            N[:, w] += z[d][w]
+            Nt += z[d, w]
+            Nd[d] += z[d, w]
+
+            return np.max(np.abs(old_z - z[d][w]))
+
+        max_diff = 0.0
+        for d in xrange(D):
+            for w in docs[d].nonzero()[0]:
+                max_diff = np.max(variational_update(d, w), max_diff)
+
+        if max_diff < epsilon:
+            break
+
+    #make theta from Nd and phi from N
+    phi = N / N.sum(axis=1)[:, np.newaxis]
+    theta = Nd / Nd.sum(axis=1)[:, np.newaxis]
+
+    return phi, theta, z
